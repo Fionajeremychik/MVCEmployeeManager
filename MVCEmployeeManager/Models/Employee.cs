e23ca17595473c45da2a9cb811a00e7be507c81d
matthew chick <chikmatthew@gmail.com>
2023-01-24 16:05:15 +1100

    Add a class Employee model and map to exisiting table

---
+++ MVCEmployeeManager/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCEmployeeManager.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Required(ErrorMessage = "Employee ID is required")]
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "Employee First Name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Employee Last Name is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Employee Title is required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Employee BirthDate is required")]
+        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "Employee HireDate is required")]
+        public DateTime HireDate { get; set; }
+        [Required(ErrorMessage = "Employee Country is required")]
+        public String Country { get; set; }
+        public string Notes { get; set; }
+    }
+}
