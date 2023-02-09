28b8db6e1fa70b37e652eebcafaaf237aaed0f3b
chikmatthew <chikmatthew@gmail.com>
2023-02-09 14:56:35 +1100

    Create Register Model and Securitycontroller and use dependency injection

---
+++ MVCEmployeeManager/Controllers/SecurityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCEmployeeManager.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
