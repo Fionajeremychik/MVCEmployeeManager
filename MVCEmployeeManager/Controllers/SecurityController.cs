2c560e16ca7209a05e600e866fa6b4f87e8bb1d0
chikmatthew <chikmatthew@gmail.com>
2023-02-09 16:17:50 +1100

    upgrade from core 3.1 to 5

--- MVCEmployeeManager/Controllers/SecurityController.cs
+++ MVCEmployeeManager/Controllers/SecurityController.cs
@@ -1,110 +1,107 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MVCEmployeeManager.Models;
 using MVCEmployeeManager.Security;
 
-namespace MVCEmployeeManager.Controllers
+namespace EmployeeManager.Controllers
 {
-    public class SecurityController : Controller
+  public class SecurityController : Controller
+  {
+    private readonly UserManager<AppIdentityUser> userManager;
+    private readonly RoleManager<AppIdentityRole> roleManager;
+    private readonly SignInManager<AppIdentityUser> signInManager;
+
+    public SecurityController(UserManager<AppIdentityUser> userManager,
+      RoleManager<AppIdentityRole> roleManager,
+      SignInManager<AppIdentityUser> signInManager)
     {
-        // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/azure-active-directory/?view=aspnetcore-7.0
-        private readonly UserManager<AppIdentityUser> userManager;
-        private readonly RoleManager<AppIdentityRole> roleManager;
-        // https://www.tektutorialshub.com/asp-net-core/asp-net-core-identity-tutorial/
-        private readonly SignInManager<AppIdentityUser> signInManager;
-
-        public SecurityController(UserManager<AppIdentityUser> userManager,
-          RoleManager<AppIdentityRole> roleManager,
-          SignInManager<AppIdentityUser> signInManager)
-        {
-            this.userManager = userManager;
-            this.roleManager = roleManager;
-            this.signInManager = signInManager;
-        }
-
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Register(Register register)
-        {
-            if (ModelState.IsValid)
-            {
-                if (!roleManager.RoleExistsAsync("Manager").Result)
-                {
-                    var role = new AppIdentityRole();
-                    role.Name = "Manager";
-                    role.Description = "Can Perform Crud Operations";
-                    var roleResult = roleManager.CreateAsync(role).Result;
-                }
-
-                var user = new AppIdentityUser();
-                user.UserName = register.UserName;
-                user.Email = register.Email;
-                user.FullName = register.FullName;
-                user.BirthDate = register.BirthDate;
-
-                var result = userManager.CreateAsync(user, register.Password).Result;
-
-                if (result.Succeeded)
-                {
-                    // https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.identity.usermanager-1.addtoroleasync?view=aspnetcore-7.0
-                    userManager.AddToRoleAsync(user, "Manager").Wait();
-                    // redirect to Security Controller (SignIn)
-                    return RedirectToAction("SignIn", "Security");
-                }
-
-                else
-                {
-                    ModelState.AddModelError("", "Invalid User Details");
-
-                }
-            }
-
-            return View(register);
-        }
-
-        [HttpGet]
-        public IActionResult SignIn()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult SignIn(SignIn signIn)
-        {
-            if (ModelState.IsValid)
-            {
-                var result = signInManager.PasswordSignInAsync(signIn.UserName, signIn.Password, signIn.RememberMe, false).Result;
-
-                if (result.Succeeded)
-                    return RedirectToAction("List", "Employees");
-                else
-                    ModelState.AddModelError("", "Invalid User Details");
-
-            }
-            return View(signIn);
-
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public IActionResult SignOut()
-        {
-            signInManager.SignOutAsync().Wait();
-            return RedirectToAction("SignIn", "Security");
-        }
-
-        public IActionResult AccessDenied()
-        {
-            return View();
-        }
+      this.userManager = userManager;
+      this.roleManager = roleManager;
+      this.signInManager = signInManager;
     }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Register(Register register)
+    {
+      if (ModelState.IsValid)
+      {
+        if (!roleManager.RoleExistsAsync("Manager").Result)
+        {
+          var role = new AppIdentityRole();
+          role.Name = "Manager";
+          role.Description = "Can Perform Crud Operations";
+          var roleResult = roleManager.CreateAsync(role).Result;
+        }
+
+        var user = new AppIdentityUser();
+        user.UserName = register.UserName;
+        user.Email = register.Email;
+        user.FullName = register.FullName;
+        user.BirthDate = register.BirthDate;
+
+        var result = userManager.CreateAsync(user, register.Password).Result;
+
+        if (result.Succeeded)
+        {
+          userManager.AddToRoleAsync(user, "Manager").Wait();
+          return RedirectToAction("SignIn", "Security");
+        }
+        else
+        {
+          ModelState.AddModelError("", "Invalid User Details");
+        }
+      }
+
+      return View(register);
+    }
+
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult SignIn(SignIn signIn)
+    {
+      if (ModelState.IsValid)
+      {
+        var result = signInManager.PasswordSignInAsync(signIn.UserName, signIn.Password, signIn.RememberMe, false).Result;
+
+        if (result.Succeeded)
+          return RedirectToAction("List", "EmployeeManager");
+        else
+          ModelState.AddModelError("", "Invalid User Details");
+      }
+
+      return View(signIn);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize]
+    public IActionResult SignOut()
+    {
+      signInManager.SignOutAsync().Wait();
+      return RedirectToAction("SignIn", "Security");
+    }
+
+    public IActionResult AccessDenied()
+    {
+      return View();
+    }  
+  }
 }
